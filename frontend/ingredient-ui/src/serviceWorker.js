2526a8268d20a3a629f4ac62d1f86047c72c9314
‘M_4_Maniac’ <mehrbod.crud19@gmail.com>
2025-08-02 17:32:46 +0330

    Fix PWA paths and enable service worker

---
+++ frontend/ingredient-ui/src/serviceWorker.js
@@ -0,0 +1,41 @@
+// src/serviceWorker.js
+const CACHE_NAME = 'nutrino-cache-v1';
+const urlsToCache = [
+  '/nutrition-calculator/',
+  '/nutrition-calculator/index.html',
+  '/nutrition-calculator/favicon.ico',
+  '/nutrition-calculator/logo192.png',
+  '/nutrition-calculator/logo512.png',
+  '/nutrition-calculator/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
