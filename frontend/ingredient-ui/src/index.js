1ece8879a0d1c2a14c54e43e8ac237ded02817a6
‘M_4_Maniac’ <mehrbod.crud19@gmail.com>
2025-09-30 17:16:14 +0330

    testing new page for submiting orders

--- frontend/ingredient-ui/src/index.js
+++ frontend/ingredient-ui/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import './i18n';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
